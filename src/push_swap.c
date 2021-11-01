d99a7ea9b6f49ad9682a7b0bd855e88414821595
Antonio jose Martinez torres <antonmar@c2r10s3.42madrid.com>
2021-11-01 17:11:07 +0100

    push_swap terminado, + solucionado con print_list

--- src/push_swap.c
+++ src/push_swap.c
@@ -6,11 +6,36 @@
 /*   By: antonmar <antonmar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/26 15:49:44 by antonmar          #+#    #+#             */
-/*   Updated: 2021/11/01 14:53:06 by antonmar         ###   ########.fr       */
+/*   Updated: 2021/11/01 17:10:55 by antonmar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/push_swap.h"
+
+void	print_list(t_plist *a_list, t_plist *b_list)
+{
+	printf("\033[0;31m A");
+	printf("\033[0;34m%20c\n", 'B');
+	printf("\033[0;31m%-20s\033[0;34m%s\n", "---", "---");
+	printf("\033[0m");
+	while (a_list || b_list)
+	{
+		if (a_list)
+		{
+			printf(" %-20d", a_list->content);
+			a_list = a_list->next;
+		}
+		else
+			printf(" %-20s", " ");
+		if (b_list)
+		{
+			printf("%d\n", b_list->content);
+			b_list = b_list->next;
+		}
+		else
+			printf("\n");
+	}
+}
 
 int	main(int argc, char **argv)
 {
